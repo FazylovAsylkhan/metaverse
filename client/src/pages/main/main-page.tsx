883a385dc9faf27fc25d5dfdf82c3ce1cd609ca2
Asylkhan Fazylov <asylkhan_fazylov@epam.com>
2023-03-06 00:47:16 +0500

    add: error boundary

--- client/src/pages/main/main-page.tsx
+++ client/src/pages/main/main-page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Preview } from '@widgets/preview';
 import { BetaTestRegistration } from '@widgets/beta-test-registration';
+import { ErrorBoundary } from '@shared/ui';
 
 export function MainPage(): JSX.Element {
   return (
-    <>
+    <ErrorBoundary>
       <Preview />
       <BetaTestRegistration />
-    </>
+    </ErrorBoundary>
   );
 }
